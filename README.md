6e4dce3bef564f8ba024b7fda160536ceeb10992
Nicholas Car <nicholas.car@csiro.au>
2018-06-24 22:27:36 +1000

    initial commit - boilerplate only

---
+++ README.md
@@ -0,0 +1,27 @@
+# ASGS Dataset
+This will soon be a Linked Data dataset containing content delivered according to the [ASGS Ontology](https://github.com/CSIRO-enviro-informatics/asgs-ontology) which mirrors the [Australian Statistical Geography Standard (ASGS)](http://www.abs.gov.au/websitedbs/D3310114.nsf/home/Australian+Statistical+Geography+Standard+(ASGS)).
+
+It will use spatial content supplied by [Web Feature Service (WFS)](http://www.opengeospatial.org/standards/wfs) services implemented by the [Australian Bureau of Statistics (ABS)](http://www.abs.gov.au).
+
+There are multiple WFSes which will all ultimately be listed but, for now, the *GetCapabilities* endpoint of the Mesh Blocks class of objects WFS is:
+
+* <https://geo.abs.gov.au/arcgis/services/ASGS2016/MB/MapServer/WFSServer?service=wfs&version=2.0.0&request=GetCapabilities>
+
+
+
+<h2>Code</h2>
+<p>The code for this application is online at <a href="https://github.com/nicholascar/mediatypes-dataset">https://github.com/CSIRO-enviro-informatics/asgs-dataset</a>.</p>
+
+<h2>License</h2>
+<p>The content of this API is licensed for use under the GNU v3 License. See the <a href="https://www.gnu.org/copyleft/gpl.html">license deed</a> for details.</p>
+
+<h2>Contacts</h2>
+<p>Designer and Implementer:</p>
+<p>
+    <strong>Nicholas Car</strong><br />
+    <em>Senior Experimental Scientist</em><br />
+    CSIRO Land &amp; Water, Environmental Informatics Group<br />
+    Brisbane, Queensland<br />
+    <a href="mailto:nicholas.car@csiro.au">nicholas.car@csiro.au</a><br />
+    ORCID: <a href="https://orcid.org/0000-0002-8742-7730">0000-0002-8742-7730</a>
+</p>
---
+++ controller/routes.py
@@ -0,0 +1,98 @@
+from flask import Blueprint, request, redirect, url_for
+from pyldapi import *
+from model.mediatype import MediaTypeRenderer
+from model.agent import AgentRenderer
+from model import sparql
+import _conf as conf
+
+routes = Blueprint('controller', __name__)
+
+
+#
+#   pages
+#
+@routes.route('/')
+def home():
+    return render_template('page_home.html')
+
+
+#
+#   registers
+#
+@routes.route('/reg/')
+def reg():
+    return RegisterOfRegistersRenderer(
+        request,
+        'http://localhost:5000/',
+        'Register of Registers',
+        'The master register of this API',
+        conf.APP_DIR + '/rofr.ttl'
+    ).render()
+
+
+@routes.route('/mediatype/')
+def mediatypes():
+    per_page = request.args.get('per_page', type=int, default=20)
+    page = request.args.get('page', type=int, default=1)
+
+    total = sparql.total_mediatypes()
+    if total is None:
+        return Response('_data store is unreachable', status=500, mimetype='text/plain')
+
+    # get list of org URIs and labels from the triplestore
+    q = '''
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX dct: <http://purl.org/dc/terms/>
+        SELECT ?uri ?label
+        WHERE {{
+            ?uri a dct:FileFormat ;
+                 rdfs:label ?label .
+        }}
+        ORDER BY ?label
+        LIMIT {}
+        OFFSET {}
+    '''.format(per_page, (page - 1) * per_page)
+    register = []
+    mediatypes = sparql.sparql_query(q)
+
+    for mediatype in mediatypes:
+        o = str(mediatype['uri']['value'])
+        l = str(mediatype['label']['value'])
+        register.append((o, l))
+
+    return RegisterRenderer(
+        request,
+        'http://localhost:5000/policy/',
+        'Register of Media Types',
+        'All the Media Types in IANA\'s list at https://www.iana.org/assignments/media-types/media-types.xml.',
+        register,
+        ['http://purl.org/dc/terms/FileFormat'],
+        total,
+        super_register='http://localhost:5000/reg/'
+    ).render()
+
+
+#
+#   instances
+#
+@routes.route('/object')
+def object():
+    if request.args.get('uri') is not None and str(request.args.get('uri')).startswith('http'):
+        uri = request.args.get('uri')
+    else:
+        return Response('You must supply the URI if a resource with ?uri=...', status=400, mimetype='text/plain')
+
+    # protecting against '+' being rendered as a space in MTs like application/rdf+xml
+    uri = uri.replace(' ', '+')
+
+    # distinguish Media Types from Agents
+    if '/' in uri.replace('https://w3id.org/mediatype/', ''):
+        return MediaTypeRenderer(request, uri).render()
+    else:
+        return AgentRenderer(request, uri).render()
+
+
+# mediatype alias
+@routes.route('/mediatype/<path:mt>')
+def mediatype_redirect(mt):
+    return redirect(url_for('controller.object', uri='https://w3id.org/mediatype/' + mt))
