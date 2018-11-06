ee29660442149807bf50ff1797077607cdefdccb
Ashley Sommer <Ashley.Sommer@csiro.au>
2018-11-06 12:13:06 +1000

    Add query string args to redirect url

--- asgs_dataset/controller/routes.py
+++ asgs_dataset/controller/routes.py
@@ -203,35 +203,42 @@ def object():
 # mediatype alias
 @routes.route('/meshblock/<path:mb>')
 def redirect_meshblock(mb):
-    return redirect(url_for('controller.object', uri=conf.URI_MESHBLOCK_INSTANCE_BASE + mb))
+    args = request.args
+    return redirect(url_for('controller.object', uri=conf.URI_MESHBLOCK_INSTANCE_BASE + mb, **args))
 
 
 # state alias
 @routes.route('/state/<path:state>')
 def redirect_state(state):
-    return redirect(url_for('controller.object', uri=conf.URI_STATE_INSTANCE_BASE + state))
+    args = request.args
+    return redirect(url_for('controller.object', uri=conf.URI_STATE_INSTANCE_BASE + state, **args))
 
 # aus alias
 @routes.route('/australia/<string:code>')
 def redirect_aus(code):
-    return redirect(url_for('controller.object', uri=conf.URI_AUS_INSTANCE_BASE + code))
+    args = request.args
+    return redirect(url_for('controller.object', uri=conf.URI_AUS_INSTANCE_BASE + code, **args))
 
 # sa1 alias
 @routes.route('/sa1/<path:sa1>')
 def redirect_sa1(sa1):
-    return redirect(url_for('controller.object', uri=conf.URI_SA1_INSTANCE_BASE + sa1))
+    args = request.args
+    return redirect(url_for('controller.object', uri=conf.URI_SA1_INSTANCE_BASE + sa1, **args))
 
 # sa2 alias
 @routes.route('/sa2/<path:sa2>')
 def redirect_sa2(sa2):
-    return redirect(url_for('controller.object', uri=conf.URI_SA2_INSTANCE_BASE + sa2))
+    args = request.args
+    return redirect(url_for('controller.object', uri=conf.URI_SA2_INSTANCE_BASE + sa2, **args))
 
 # sa3 alias
 @routes.route('/sa3/<path:sa3>')
 def redirect_sa3(sa3):
-    return redirect(url_for('controller.object', uri=conf.URI_SA3_INSTANCE_BASE + sa3))
+    args = request.args
+    return redirect(url_for('controller.object', uri=conf.URI_SA3_INSTANCE_BASE + sa3, **args))
 
 # sa4 alias
 @routes.route('/sa4/<path:sa4>')
 def redirect_sa4(sa4):
-    return redirect(url_for('controller.object', uri=conf.URI_SA4_INSTANCE_BASE + sa4))
+    args = request.args
+    return redirect(url_for('controller.object', uri=conf.URI_SA4_INSTANCE_BASE + sa4, **args))
