6bbdae07042b149cc3e0808b87e00d2fa2900ad9
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-08-15 21:14:21 +1000

    Adding command line options to run app only in setup mode

--- asgs_dataset/app.py
+++ asgs_dataset/app.py
@@ -3,6 +3,7 @@ import asgs_dataset._config as conf
 from flask import Flask
 from asgs_dataset.controller import routes
 import pyldapi
+import argparse
 
 app = Flask(__name__, template_folder=conf.TEMPLATES_DIR, static_folder=conf.STATIC_DIR)
 app.register_blueprint(routes.routes)
@@ -10,6 +11,10 @@ app.register_blueprint(routes.routes)
 
 # run the Flask app
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='ASGS Dataset LDAPI')
+    parser.add_argument('--init', action="store_true", default=False, help='Initialise the application then exit (rofr.ttl etc)')
+    args, unknown = parser.parse_known_args()
+
     logging.basicConfig(filename=conf.LOGFILE,
                         level=logging.DEBUG,
                         datefmt='%Y-%m-%d %H:%M:%S',
@@ -18,5 +23,6 @@ if __name__ == '__main__':
     pyldapi.setup(app, conf.APP_DIR, conf.DATA_URI_PREFIX)
 
     # run the Flask app
-    app.run(debug=conf.DEBUG, threaded=True, use_reloader=False)
+    if not args.init:
+        app.run(debug=conf.DEBUG, threaded=True, use_reloader=False)
 
