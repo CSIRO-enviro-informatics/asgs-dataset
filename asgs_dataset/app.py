41e717a36a043d7682dcd1e9e986bd9ca3cf336c
Jonathan Yu <jonathan.yu@csiro.au>
2020-02-27 22:15:14 +1100

    add include_raw function

--- asgs_dataset/app.py
+++ asgs_dataset/app.py
@@ -4,10 +4,22 @@ from flask import Flask
 from asgs_dataset.controller import controller
 import pyldapi
 import argparse
+from jinja2 import Markup
 
 app = Flask(__name__, template_folder=conf.TEMPLATES_DIR, static_folder=conf.STATIC_DIR)
 app.register_blueprint(controller)
 
+@app.context_processor
+def utility_processor():
+    def include_raw(url):
+        import urllib.request
+        import json
+        res = urllib.request.urlopen(url)
+        res_body = res.read()
+        jo = json.loads(res_body.decode("utf-8"))
+        j = json.dumps(jo, indent=4)
+        return Markup(j)
+    return dict(include_raw=include_raw)
 
 def run():
     parser = argparse.ArgumentParser(description='ASGS Dataset LDAPI')
